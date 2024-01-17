a7aa1e35e092b91808730b3810ce202796492486
rqure <64617806+rqure@users.noreply.github.com>
2024-01-17 16:55:40 -0700

    Update application.go

--- src/application.go
+++ src/application.go
@@ -1,8 +1,46 @@
 package qmq
 
+import (
+	"context"
+	"os"
+	"strconv"
+)
+
 type QMQApplication struct {
 	conn      *QMQConnection
 	producers map[string]*QMQProducer
 	consumers map[string]*QMQConsumer
 	logger    *QMQLogger
+}
+
+func NewQMQApplication(ctx context.Context, name string) *QMQApplication {
+	addr := os.Getenv("QMQ_ADDR")
+	if addr == "" {
+		addr = "redis:6379"
+	}
+	
+	password := os.Getenv("QMQ_PASSWORD")
+	
+	logLength, err := strconv.Atoi(os.Getenv("QMQ_LOG_LENGTH"))
+	if err != nil {
+		logLength = 100
+	}
+	
+	conn := NewQMQConnection(addr, password)
+	
+	return &QMQApplication{
+		conn: conn,
+		logger: NewQMQLogger(ctx, name, conn, logLength)
+	}
+}
+
+func (a *QMQApplication) Initialize(ctx context.Context) {
+	a.conn.Connect(ctx)
+	a.logger.Initialize(ctx)
+	a.logger.Advise(ctx, "Application has started")
+}
+
+func (a *QMQApplication) Deinitialize(ctx context.Context) {
+	a.logger.Advise(ctx, "Application has stopped")
+	a.conn.Disconnect()
 }
