99c1536118fdca4ed9204b3b9013472ca2906e3c
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-01-16 22:19:18 -0700

    some progresS

--- src/producer.go
+++ src/producer.go
@@ -1,13 +1,18 @@
 package qmq
 
-import "context"
+import (
+	"context"
+	"time"
+
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
 
 type QMQProducer struct {
 	conn   *QMQConnection
 	stream *QMQStream
 }
 
-func NewQMQProducer(ctx context.Context, key string, conn *QMQConnection) *QMQProducer {
+func NewQMQProducer(ctx context.Context, key string, conn *QMQConnection, length int64) *QMQProducer {
 	producer := &QMQProducer{
 		conn:   conn,
 		stream: NewQMQStream(key, conn),
@@ -18,9 +23,16 @@ func NewQMQProducer(ctx context.Context, key string, conn *QMQConnection) *QMQPr
 		readRequest.Data.UnmarshalTo(&producer.stream.Context)
 	}
 
+	producer.stream.Length = length
+
 	return producer
 }
 
-func (p *QMQProducer) push(ctx context.Context) {
+func (p *QMQProducer) push(ctx context.Context, m protoreflect.ProtoMessage) {
+	for !p.stream.Locker.Lock(ctx) {
+		time.Sleep(100 * time.Millisecond)
+	}
+	defer p.stream.Locker.Unlock(ctx)
 
+	p.conn.StreamAdd(ctx, p.stream, m)
 }
