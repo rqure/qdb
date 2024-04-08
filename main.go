bb7abccef325e10b4891c3415e4f2348bf4ef588
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-04-07 19:06:21 -0600

    some progress

--- main.go
+++ main.go
@@ -4,7 +4,15 @@ import (
 	qmq "github.com/rqure/qmq/src"
 )
 
+type ExampleNameProvider struct{}
+
+func (p *ExampleNameProvider) Get() string {
+	return "example"
+}
+
 func main() {
-	engine := qmq.NewDefaultEngine()
+	engine := qmq.NewDefaultEngine(qmq.DefaultEngineConfig{
+		NameProvider: &ExampleNameProvider{},
+	})
 	engine.Run()
 }
