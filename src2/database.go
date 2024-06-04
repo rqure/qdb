2a77f1802918519eec5bbdf95e60515cd38c46b3
rqure <64617806+rqure@users.noreply.github.com>
2024-06-04 22:08:59 +0000

    some progress

--- src2/database.go
+++ src2/database.go
@@ -8,6 +8,7 @@ import (
 	"github.com/redis/go-redis/v9"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type IDatabase interface {
@@ -19,17 +20,17 @@ type IDatabase interface {
 
 	FindEntities(entityType string) []string
 
-	GetEntityFieldType(fieldName string) *anypb.Any
-	SetEntityFieldType(fieldName string, value proto.Message)
+	GetFieldSchema(fieldName string) *DatabaseFieldSchema
+	SetFieldSchema(fieldName string, value *DatabaseFieldSchema)
 
-	GetEntityDefinition(entityType string) *EntityDefinition
-	SetEntityDefinition(entityType string, definition *EntityDefinition)
+	GetEntitySchema(entityType string) *DatabaseEntitySchema
+	SetEntitySchema(entityType string, value *DatabaseEntitySchema)
 
-	ReadEntityFields(requests []*EntityRequest)
-	WriteEntityFields(requests []*EntityRequest)
+	Read(requests []*DatabaseRequest)
+	Write(requests []*DatabaseRequest)
 
-	SubscribeOnFieldChanges(entityIdOrEntityType string, entityField string, change bool, contextFields []string, callback func(*EntityFieldNotification)) string
-	UnsubscribeFromFieldChanges(subscriptionId string)
+	Notify(config *DatabaseNotificationConfig, callback func(*DatabaseNotification)) string
+	Unnotify(subscriptionId string)
 }
 
 type RedisDatabaseConfig struct {
@@ -37,9 +38,48 @@ type RedisDatabaseConfig struct {
 	Password string
 }
 
+// schema:entity:<type> -> DatabaseEntitySchema
+// schema:field:<name> -> DatabaseFieldSchema
+// instance:entity:<entityId> -> DatabaseEntity
+// instance:field:<name>:<entityId> -> DatabaseField
+// instance:type:<entityType> -> []string{entityId...}
+// instance:notification:<entityId>:<fieldName> -> []string{subscriptionId...}
+// instance:notification:<entityType>:<fieldName> -> []string{subscriptionId...}
+type RedisDatabaseKeyGenerator struct{}
+
+func (g *RedisDatabaseKeyGenerator) GetEntitySchemaKey(entityType string) string {
+	return "schema:entity:" + entityType
+}
+
+func (g *RedisDatabaseKeyGenerator) GetFieldSchemaKey(fieldName string) string {
+	return "schema:field:" + fieldName
+}
+
+func (g *RedisDatabaseKeyGenerator) GetEntityKey(entityId string) string {
+	return "instance:entity:" + entityId
+}
+
+func (g *RedisDatabaseKeyGenerator) GetFieldKey(fieldName, entityId string) string {
+	return "instance:field:" + fieldName + ":" + entityId
+}
+
+func (g *RedisDatabaseKeyGenerator) GetEntityTypeKey(entityType string) string {
+	return "instance:type:" + entityType
+}
+
+func (g *RedisDatabaseKeyGenerator) GetEntityIdNotificationKey(entityId, fieldName string) string {
+	return "instance:notification:" + entityId + ":" + fieldName
+}
+
+func (g *RedisDatabaseKeyGenerator) GetEntityTypeNotificationKey(entityType, fieldName string) string {
+	return "instance:notification:" + entityType + ":" + fieldName
+}
+
 type RedisDatabase struct {
-	client *redis.Client
-	config RedisDatabaseConfig
+	client    *redis.Client
+	config    RedisDatabaseConfig
+	callbacks map[string]func(*DatabaseNotification)
+	keygen    RedisDatabaseKeyGenerator
 }
 
 func (db *RedisDatabase) Connect() {
@@ -65,14 +105,13 @@ func (db *RedisDatabase) CreateEntity(entityType, parentId, name string) {
 	entityId := uuid.New().String()
 
 	requests := []*EntityRequest{}
-	for _, fieldName := range db.GetEntityDefinition(entityType).EntityFieldNames {
+	for _, fieldName := range db.GetEntitySchema(entityType).EntityFieldNames {
 		requests = append(requests, &EntityRequest{
 			EntityId:  entityId,
 			FieldName: fieldName,
-			Value:     db.GetEntityFieldType(fieldName),
 		})
 	}
-	db.WriteEntityFields(requests)
+	db.Write(requests)
 
 	p := &Entity{
 		Id:          entityId,
@@ -86,12 +125,12 @@ func (db *RedisDatabase) CreateEntity(entityType, parentId, name string) {
 		return
 	}
 
-	db.client.SAdd(context.Background(), "schema:entityType:"+entityType, entityId)
-	db.client.Set(context.Background(), "schema:entity:"+entityId, b, 0)
+	db.client.SAdd(context.Background(), db.keygen.GetEntityTypeKey(entityType), entityId)
+	db.client.Set(context.Background(), db.keygen.GetEntityKey(entityId), b, 0)
 }
 
 func (db *RedisDatabase) DeleteEntity(entityId string) {
-	e, err := db.client.Get(context.Background(), "schema:entity:"+entityId).Result()
+	e, err := db.client.Get(context.Background(), db.keygen.GetEntityKey(entityId)).Result()
 	if err != nil {
 		return
 	}
@@ -105,28 +144,25 @@ func (db *RedisDatabase) DeleteEntity(entityId string) {
 		return
 	}
 
-	// TODO Remove children
 	for _, childrenId := range p.ChildrenIds {
 		db.DeleteEntity(childrenId)
 	}
 
-	// TODO Remove fields
-	d := db.GetEntityDefinition(p.Type)
-	for _, fieldName := range d.EntityFieldNames {
-		db.client.Del(context.Background(), "entity:"+entityId+":fieldType:"+fieldName)
+	for _, fieldName := range db.GetEntitySchema(p.Type).EntityFieldNames {
+		db.client.Del(context.Background(), db.keygen.GetFieldKey(fieldName, entityId))
 	}
 
-	db.client.SRem(context.Background(), "schema:entityType:"+p.Type, entityId)
-	db.client.Del(context.Background(), "schema:entity:"+entityId)
+	db.client.SRem(context.Background(), db.keygen.GetEntityTypeKey(p.Type), entityId)
+	db.client.Del(context.Background(), db.keygen.GetEntityKey(entityId))
 }
 
 func (db *RedisDatabase) FindEntities(entityType string) []string {
-
+	return db.client.SMembers(context.Background(), db.keygen.GetEntityTypeKey(entityType)).Val()
 }
 
-func (db *RedisDatabase) GetEntityFieldType(fieldName string) *anypb.Any {
+func (db *RedisDatabase) GetFieldSchema(fieldName string) *DatabaseFieldSchema {
 	a := &anypb.Any{}
-	e, err := db.client.Get(context.Background(), "schema:fieldType:"+fieldName).Result()
+	e, err := db.client.Get(context.Background(), db.keygen.GetFieldSchemaKey(fieldName)).Result()
 	if err != nil {
 		return a
 	}
@@ -136,11 +172,15 @@ func (db *RedisDatabase) GetEntityFieldType(fieldName string) *anypb.Any {
 		return a
 	}
 
-	proto.Unmarshal(b, a)
+	err = proto.Unmarshal(b, a)
+	if err != nil {
+		return nil
+	}
+
 	return a
 }
 
-func (db *RedisDatabase) SetEntityFieldType(fieldName string, value proto.Message) {
+func (db *RedisDatabase) SetFieldSchema(fieldName string, value *DatabaseFieldSchema) {
 	a, err := anypb.New(value)
 	if err != nil {
 		return
@@ -151,11 +191,11 @@ func (db *RedisDatabase) SetEntityFieldType(fieldName string, value proto.Messag
 		return
 	}
 
-	db.client.Set(context.Background(), "schema:fieldType:"+fieldName, base64.StdEncoding.EncodeToString(b), 0)
+	db.client.Set(context.Background(), db.keygen.GetFieldSchemaKey(fieldName), base64.StdEncoding.EncodeToString(b), 0)
 }
 
-func (db *RedisDatabase) GetEntityDefinition(entityType string) *EntityDefinition {
-	e, err := db.client.Get(context.Background(), "schema:entityDefinition:"+entityType).Result()
+func (db *RedisDatabase) GetEntitySchema(entityType string) *DatabaseEntitySchema {
+	e, err := db.client.Get(context.Background(), db.keygen.GetEntitySchemaKey(entityType)).Result()
 	if err != nil {
 		return nil
 	}
@@ -165,7 +205,7 @@ func (db *RedisDatabase) GetEntityDefinition(entityType string) *EntityDefinitio
 		return nil
 	}
 
-	p := &EntityDefinition{}
+	p := &DatabaseEntitySchema{}
 	err = proto.Unmarshal(b, p)
 	if err != nil {
 		return nil
@@ -174,27 +214,102 @@ func (db *RedisDatabase) GetEntityDefinition(entityType string) *EntityDefinitio
 	return p
 }
 
-func (db *RedisDatabase) SetEntityDefinition(entityType string, definition *EntityDefinition) {
-	b, err := proto.Marshal(definition)
+func (db *RedisDatabase) SetEntitySchema(entityType string, value *DatabaseEntitySchema) {
+	b, err := proto.Marshal(value)
 	if err != nil {
 		return
 	}
 
-	db.client.Set(context.Background(), "schema:entityDefinition:"+entityType, base64.StdEncoding.EncodeToString(b), 0)
+	db.client.Set(context.Background(), db.keygen.GetEntitySchemaKey(entityType), base64.StdEncoding.EncodeToString(b), 0)
 }
 
-func (db *RedisDatabase) ReadEntityFields(requests []*EntityRequest) {
+func (db *RedisDatabase) Read(requests []*DatabaseRequest) {
+	for _, request := range requests {
+		e, err := db.client.Get(context.Background(), db.keygen.GetFieldKey(request.Field, request.Id)).Result()
+		if err != nil {
+			continue
+		}
 
+		b, err := base64.StdEncoding.DecodeString(e)
+		if err != nil {
+			continue
+		}
+
+		p := &EntityField{}
+		err = proto.Unmarshal(b, p)
+		if err != nil {
+			continue
+		}
+
+		request.Value = p.Value
+		request.WriteTime.Raw = p.WriteTime
+		request.WriterId.Raw = p.WriterId
+	}
 }
 
-func (db *RedisDatabase) WriteEntityFields(requests []*EntityRequest) {
+func (db *RedisDatabase) Write(requests []*DatabaseRequest) {
+	for _, request := range requests {
+		valueDef := db.GetFieldSchema(request.FieldName)
+		if request.Value == nil || request.Value.TypeUrl != valueDef.Type {
+			request.Value = valueDef
+		}
 
+		if request.WriteTime == nil {
+			request.WriteTime = &Timestamp{Raw: timestamppb.Now()}
+		}
+
+		if request.WriterId == nil {
+			request.WriterId = &String{Raw: ""}
+		}
+
+		p := &EntityField{
+			EntityId:  request.EntityId,
+			Name:      request.FieldName,
+			Value:     request.Value,
+			WriteTime: request.WriteTime.Raw,
+			WriterId:  request.WriterId.Raw,
+		}
+
+		b, err := proto.Marshal(p)
+		if err != nil {
+			continue
+		}
+
+		db.client.Set(context.Background(), db.keygen.GetFieldKey(request.Field, request.Id), base64.StdEncoding.EncodeToString(b), 0)
+	}
 }
 
-func (db *RedisDatabase) SubscribeOnFieldChanges(entityIdOrEntityType string, entityField string, change bool, contextFields []string, callback func(EntityFieldNotification)) string {
+func (db *RedisDatabase) Notify(config *DatabaseNotificationConfig, callback func(*DatabaseNotification)) string {
+	if db.GetFieldSchema(config.Field) == nil {
+		return ""
+	}
 
+	b, err := proto.Marshal(config)
+	if err != nil {
+		return ""
+	}
+
+	e := base64.StdEncoding.EncodeToString(b)
+
+	if config.Id != "" {
+		db.client.SAdd(context.Background(), db.keygen.GetEntityIdNotificationKey(config.Id, config.Field), e)
+		db.callbacks[e] = callback
+		return e
+	}
+
+	if config.Type != "" {
+		db.client.SAdd(context.Background(), db.keygen.GetEntityTypeNotificationKey(config.Type, config.Field), e)
+		db.callbacks[e] = callback
+		return e
+	}
+
+	return ""
 }
 
-func (db *RedisDatabase) UnsubscribeFromFieldChanges(subscriptionId string) {
+func (db *RedisDatabase) Unnotify(subscriptionId string) {
+	if db.callbacks[subscriptionId] == nil {
+		return
+	}
 
+	delete(db.callbacks, subscriptionId)
 }
