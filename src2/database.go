584e70463aa090b89fa23d18b8304c34fbfc7399
rqure <64617806+rqure@users.noreply.github.com>
2024-06-04 18:06:29 +0000

    some progress

--- src2/database.go
+++ src2/database.go
@@ -22,13 +22,13 @@ type IDatabase interface {
 	GetEntityFieldType(fieldName string) *anypb.Any
 	SetEntityFieldType(fieldName string, value proto.Message)
 
-	GetEntityDefinition(entityType string) EntityDefinition
-	SetEntityDefinition(entityType string, definition EntityDefinition)
+	GetEntityDefinition(entityType string) *EntityDefinition
+	SetEntityDefinition(entityType string, definition *EntityDefinition)
 
-	ReadEntityFields(requests []EntityRequest)
-	WriteEntityFields(requests []EntityRequest)
+	ReadEntityFields(requests []*EntityRequest)
+	WriteEntityFields(requests []*EntityRequest)
 
-	SubscribeOnFieldChanges(entityIdOrEntityType string, entityField string, change bool, contextFields []string, callback func(EntityFieldNotification)) string
+	SubscribeOnFieldChanges(entityIdOrEntityType string, entityField string, change bool, contextFields []string, callback func(*EntityFieldNotification)) string
 	UnsubscribeFromFieldChanges(subscriptionId string)
 }
 
@@ -64,19 +64,60 @@ func (db *RedisDatabase) Disconnect() {
 func (db *RedisDatabase) CreateEntity(entityType, parentId, name string) {
 	entityId := uuid.New().String()
 
-	requests := []EntityRequest{}
+	requests := []*EntityRequest{}
 	for _, fieldName := range db.GetEntityDefinition(entityType).EntityFieldNames {
-		requests = append(requests, EntityRequest{
+		requests = append(requests, &EntityRequest{
 			EntityId:  entityId,
 			FieldName: fieldName,
 			Value:     db.GetEntityFieldType(fieldName),
 		})
 	}
 	db.WriteEntityFields(requests)
+
+	p := &Entity{
+		Id:          entityId,
+		Name:        name,
+		ParentId:    parentId,
+		Type:        entityType,
+		ChildrenIds: []string{},
+	}
+	b, err := proto.Marshal(p)
+	if err != nil {
+		return
+	}
+
+	db.client.SAdd(context.Background(), "schema:entityType:"+entityType, entityId)
+	db.client.Set(context.Background(), "schema:entity:"+entityId, b, 0)
 }
 
 func (db *RedisDatabase) DeleteEntity(entityId string) {
+	e, err := db.client.Get(context.Background(), "schema:entity:"+entityId).Result()
+	if err != nil {
+		return
+	}
+	b, err := base64.StdEncoding.DecodeString(e)
+	if err != nil {
+		return
+	}
+	p := &Entity{}
+	err = proto.Unmarshal(b, p)
+	if err != nil {
+		return
+	}
 
+	// TODO Remove children
+	for _, childrenId := range p.ChildrenIds {
+		db.DeleteEntity(childrenId)
+	}
+
+	// TODO Remove fields
+	d := db.GetEntityDefinition(p.Type)
+	for _, fieldName := range d.EntityFieldNames {
+		db.client.Del(context.Background(), "entity:"+entityId+":fieldType:"+fieldName)
+	}
+
+	db.client.SRem(context.Background(), "schema:entityType:"+p.Type, entityId)
+	db.client.Del(context.Background(), "schema:entity:"+entityId)
 }
 
 func (db *RedisDatabase) FindEntities(entityType string) []string {
@@ -113,19 +154,40 @@ func (db *RedisDatabase) SetEntityFieldType(fieldName string, value proto.Messag
 	db.client.Set(context.Background(), "schema:fieldType:"+fieldName, base64.StdEncoding.EncodeToString(b), 0)
 }
 
-func (db *RedisDatabase) GetEntityDefinition(entityType string) EntityDefinition {
+func (db *RedisDatabase) GetEntityDefinition(entityType string) *EntityDefinition {
+	e, err := db.client.Get(context.Background(), "schema:entityDefinition:"+entityType).Result()
+	if err != nil {
+		return nil
+	}
+
+	b, err := base64.StdEncoding.DecodeString(e)
+	if err != nil {
+		return nil
+	}
+
+	p := &EntityDefinition{}
+	err = proto.Unmarshal(b, p)
+	if err != nil {
+		return nil
+	}
+
+	return p
+}
+
+func (db *RedisDatabase) SetEntityDefinition(entityType string, definition *EntityDefinition) {
+	b, err := proto.Marshal(definition)
+	if err != nil {
+		return
+	}
+
+	db.client.Set(context.Background(), "schema:entityDefinition:"+entityType, base64.StdEncoding.EncodeToString(b), 0)
+}
+
+func (db *RedisDatabase) ReadEntityFields(requests []*EntityRequest) {
 
 }
 
-func (db *RedisDatabase) SetEntityDefinition(entityType string, definition EntityDefinition) {
-
-}
-
-func (db *RedisDatabase) ReadEntityFields(requests []EntityRequest) {
-
-}
-
-func (db *RedisDatabase) WriteEntityFields(requests []EntityRequest) {
+func (db *RedisDatabase) WriteEntityFields(requests []*EntityRequest) {
 
 }
 
