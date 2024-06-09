046d0bcf34f76b23f4e42b9715485e50bf75e68f
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-06-08 22:58:30 -0600

    some progress

--- src/web_service_worker.go
+++ src/web_service_worker.go
@@ -37,8 +37,6 @@ func (w *WebServiceWorker) Init() {
 	// Handle WebSocket and other routes
 	http.Handle("/", w)
 
-	Register_web_handler_notification_listener()
-	Register_web_handler_notification_manager()
 	Register_web_handler_server_interactor()
 	Register_web_handler_app()
 
